f17f0717b734723403fe981b5b1d157f5e621ee7
UrsaZzzMinor <ava70390@gmail.com>
2016-10-10 23:02:14 +0300

    modifying some issues in Product class

--- task2/task3/src/classes/Product.java
+++ task2/task3/src/classes/Product.java
@@ -5,18 +5,25 @@ import interfaces.IProductPart;
 
 public class Product implements IProduct{
 	
+	TankBody bodyProd = new TankBody();
+	TankEngine engineProd = new TankEngine();
+	TankTower towerProd = new TankTower();
+	
 	@Override
 	public void installFirstPart(IProductPart part) {
+		this.bodyProd = (TankBody) part;
 		System.out.println(part.getName() + " has been installed!");
 	}
 
 	@Override
 	public void installSecondPart(IProductPart part) {
+		this.engineProd = (TankEngine) part;
 		System.out.println(part.getName() + " has been installed!");		
 	}
 
 	@Override
 	public void installThirdPart(IProductPart part) {
+		this.towerProd = (TankTower) part;
 		System.out.println(part.getName() + " has been installed!");
 	}
 	
